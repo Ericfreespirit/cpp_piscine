7dd23019deab1a6f8045aa5021f03e6b83affed4
Eric LING <eriling@e2r5p8.clusters.42paris.fr>
2022-01-20 15:08:57 +0100

    begin M08

--- M07/ex02/Array.hpp
+++ M07/ex02/Array.hpp
@@ -2,25 +2,55 @@
 #define ARRAY_H
 
 #include <iostream>
+#include <exception>
 
 template <typename T>
 class Array{
+public:
+    class IndexTooHigh : public std::exception {
+        public:
+        const char *what() const throw(){
+            return ("index too high compared to array size");
+        };
+    };
 
 public:
-    Array(){};
-    Array(unsigned int n){
-        for(int i = 0;i < n; i++){
-            T *a = new T();
-            std::cout << *a << std::end;
-        }
+
+    Array(){
+        this->_arr = new T[0]();
+        this->_sz = 0;
     };
-    Array(const Array &ref){
+    Array(unsigned int n){
+        this->_arr = new T[n]();
+        this->_sz = n;
+    };
+    Array(const Array<T> &ref){
         *this = ref;
     };
-
-    Array &operator=(const Array *ref){
-
+    ~Array(){
+            delete[] this->_arr;
+    }
+    Array<T> &operator=(const Array<T> &ref){
+        this->_sz = ref._sz;
+        this->_arr = new T[this->_sz];
+        return(*this);
     };
 
+
+    int size(){
+        return (this->_sz);
+    };
+
+    T &operator[](int index){
+        if (index + 1 > size())
+            throw Array<T>::IndexTooHigh();
+        return (this->_arr[index]);
+    };
+
+private:
+    T *_arr;
+    int _sz;
+     
+};
 
 #endif
--- M07/ex02/main.cpp
+++ M07/ex02/main.cpp
@@ -2,7 +2,36 @@
 
 int main()
 {
+    Array <int> my_intArr(10);
+    Array <char> my_charArr(2);
 
+    std::cout << std::endl;
+    std::cout << "== Test 1 ==" << std::endl;
+    std::cout << std::endl;
+    try {
+         std::cout << "arr: {" << &my_intArr[0]<< "}" << std::endl;
+        std::cout << "arr: {" << my_intArr[0]<< "}" << std::endl;
+        std::cout << "arr: {" << &my_intArr[9]<< "}" << std::endl;
+        std::cout << "arr: {" << my_intArr[9]<< "}" << std::endl;
+        std::cout << my_intArr[10];
+    }
+    catch (std::exception &e){
+        std::cerr << e.what() << std::endl;
+    }
 
+    std::cout << std::endl;
+    std::cout << "== Test 2 ==" << std::endl;
+    std::cout << std::endl;
+    try {
+        std::cout << "arr: {"<< static_cast<void*> (&my_charArr[0]) << "}" << std::endl;
+        std::cout << "arr: {"<< my_charArr[0] << "}" << std::endl;
+        std::cout << "arr: {"<< static_cast<void*> (&my_charArr[1]) << "}" << std::endl;
+        std::cout << "arr: {"<< my_charArr[1] << "}" << std::endl;        
+        std::cout << my_charArr[3];
+    }
+    catch (std::exception &e){
+        std::cerr << e.what() << std::endl;
+    }
+    
     return (0);
 }
